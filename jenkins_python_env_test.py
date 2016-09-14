ce87a68628daf97099a71811f90cb193980f4308
Roshan Nagekar <roshangn88@gmail.com>
2016-09-14 11:42:06 +0530

    Check env variable access for jenkins job

--- jenkins_python_env_test.py
+++ jenkins_python_env_test.py
@@ -3,4 +3,4 @@ import os
 
 print "AWS_INSTANCE_TYPE :: %s"%os.environ["AWS_INSTANCE_TYPE"]
 print "AWS_HOSTNAME :: %s"%os.environ["AWS_HOSTNAME"]
-print "SERVER IP :: %s"%os.environ["SERVER_IP"]
+print "SERVER IP :: %s"%os.environ["SERVER"]
